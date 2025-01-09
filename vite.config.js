acb5f7e6d3bdd49f2f05a2b7cd0baf71762900ae
shivani1410551 <charanshivani8@gmail.com>
2025-01-09 23:10:36 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Vercel deployment base path
 export default defineConfig({
   plugins: [react()],
-  base: '/interactiveRatingCard/', // Base path for GitHub Pages
-  build: {
-    outDir: 'dist', // Specify the output directory for the build
-  },
+  base: '/interactiveRatingCard/', // Make sure this matches your project structure
 });
