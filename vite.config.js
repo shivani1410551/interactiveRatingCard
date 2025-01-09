b1fcf80da09a8b1a94451d64b3a222d12dcf7a0e
shivani1410551 <charanshivani8@gmail.com>
2025-01-09 22:35:45 +0530

    final commit 4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/interactiveRatingCard/'
-})
+  base: '/interactiveRatingCard/', // Base path for GitHub Pages
+  build: {
+    outDir: 'dist', // Specify the output directory for the build
+  },
+});
