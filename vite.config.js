6f74647ac90615ad5b0f2a04af4f22185625d0ee
shivani1410551 <charanshivani8@gmail.com>
2025-01-09 23:20:21 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Vercel deployment base path
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/interactiveRatingCard/', // Make sure this matches your project structure
+  base: '/', // Use '/' for the root domain
 });
